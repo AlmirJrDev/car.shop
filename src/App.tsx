96653f9d71f4398b3145d9a2c1b6109ef5d887db
Almir Jr <99154364+AlmirJrDev@users.noreply.github.com>
2024-01-23 08:04:35 -0300

    feat: global styles e default themes and fonts

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,20 @@ import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 
 import { router } from './routes'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
-    <HelmetProvider>
-      <Helmet titleTemplate="%s | car.shop" />
-      <RouterProvider router={router} />
+    
+    <ThemeProvider theme={defaultTheme}>
+        <GlobalStyle />
+        <HelmetProvider>
+        <Helmet titleTemplate="%s | car.shop" />
+        <RouterProvider router={router} />
     </HelmetProvider>
+     
+    </ThemeProvider>
   )
 }
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,22 @@
+export const defaultTheme = {
+  colors: {
+    white: '#FFFFFF',
+    background: 'linear-gradient(to bottom, #2dd4c2, #115e56)',
+    'base-card': '#272221',
+    'base-input': '#403937',
+    'base-button': '#14b8a6',
+    'base-hover': '#8D8686',
+    'base-label': '#D7D5D5',
+    'base-text': '#E6E6E5',
+    'base-subtitle': '#272221',
+    'base-title': '#272221',
+
+    'gray-light': '#EBE5F9',
+    green: '#0d9485',
+    'green-light': '#14b8a6',
+
+    'teal-light': '#5eeadb',
+    teal: '#115e56',
+    'teal-dark': '#042f2a',
+  },
+}
